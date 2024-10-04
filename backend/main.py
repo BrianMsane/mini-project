a078c1eb696946bcb064f1450dab59e39dd09bc8
Brian Msane <msanebrianboss@gmail.com>
2024-10-04 16:27:51 +0200

    backend only runs on main.py now

--- backend/main.py
+++ backend/main.py
@@ -7,10 +7,32 @@
 5. return to the user the subjects they qualify for through the website(dynamic page)
 '''
 
-from routers.func import app
+'''
+API to serve backend resources
+    Severless environment (Azure or AWS) at deployment
+    Uvicorn/local Sever for testing
+'''
+
 import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from support.emails import send_email
+from routers.api_scripts import router
+
+
+app = FastAPI(
+    title="Document OCR and Question Answering!",
+    description="This is an API that deals with Document Summarization"
+)
+app.include_router(router=router, prefix="/api")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 if __name__ == '__main__':
     uvicorn.run(app, port=3017, host='0.0.0.0')
-
